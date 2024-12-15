89380ebd7af216978a28f34a7b1521fbd0e724e3
sandeep <vattyam.sandeep@gmail.com>
2024-12-15 11:57:31 +0530

    Add header, total price, company and logo

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=10, style="B")
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=8, txt=columns[0], border=1)
-    pdf.cell(w=30, h=8, txt=columns[1], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
     pdf.cell(w=30, h=8, txt=columns[2], border=1)
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
@@ -36,11 +36,28 @@ for filepath in filepaths:
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    total_sum = df['total_price'].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=14)
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
 
